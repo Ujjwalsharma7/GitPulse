9d279dd88ce442a409e6392d6e448d2a3e71c368
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-06 23:58:42 +0530

    added components to homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import Spinner from "../components/Spinner";
 
-const Homepage = () => {
-  return <div>Homepage</div>;
+const HomePage = () => {
+  return (
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+        {/* <Spinner /> */}
+      </div>
+    </div>
+  );
 };
 
-export default Homepage;
+export default HomePage;
