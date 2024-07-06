0d63117bac09b36a343d4992553f36033f0ceb18
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-06 18:05:51 +0530

    project setup

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className=" flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen overflow-y-auto border-r py-8 bg-glass"></aside>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/pages/LikesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LikesPage = () => {
+  return <div></div>;
+};
+
+export default LikesPage;
