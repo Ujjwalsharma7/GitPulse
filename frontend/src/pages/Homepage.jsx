223b531adb4778b4bdb4547ab657c5099958917d
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-07 23:46:37 +0530

    added func to fetch user data

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,18 +1,49 @@
+import { useCallback, useEffect, useState } from "react";
 import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
 import SortRepos from "../components/SortRepos";
 import Spinner from "../components/Spinner";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [sortType, setSortType] = useState("forks");
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch("https://api.github.com/users/Ujjwalsharma7");
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+
+      const repoRes = await fetch(userProfile.repos_url);
+      const Repos = await repoRes.json();
+      setRepos(Repos);
+      console.log("user profile: ", userProfile);
+      console.log("repos: ", Repos);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
   return (
     <div className="m-4">
       <Search />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <ProfileInfo />
-        <Repos />
-        {/* <Spinner /> */}
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
