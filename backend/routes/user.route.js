9b4353cd12c7fb5ca9df1418174de71d978c82da
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-08 13:55:46 +0530

    added user profile route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getProfileAndRepos } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/profile", (req, res) => {
-  res.send("User profile");
-});
+router.get("/profile/:username", getProfileAndRepos);
 
 export default router;
