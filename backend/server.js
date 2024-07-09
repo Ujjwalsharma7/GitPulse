df59ff4307ad8ecbd5719bf9c15a5b9b2efac30b
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-09 23:50:08 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
 });
