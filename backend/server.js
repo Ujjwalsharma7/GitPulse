9a1191af219b372b11057d00f3e45bc5c58aca69
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-14 16:30:01 +0530

    test commit

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+  console.log("Server is running on port 5000");
   connectMongoDB();
 });
