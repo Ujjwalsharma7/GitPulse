fefc038378a3a4fbe38be3e2c35de2b5ce60f6cb
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-14 16:47:27 +0530

    added session and passport

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,21 @@ import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import passport from "passport";
+import "./passport/github.auth.js";
+import session from "express-session";
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
