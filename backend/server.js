3181805ef9079f0f07c1409b0a8e124e381264f8
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2024-07-16 12:54:23 +0530

    added build

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,14 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import passport from "passport";
 import "./passport/github.auth.js";
 import session from "express-session";
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+// console.log("dirname:", __dirname);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -21,14 +25,21 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
-app.listen(5000, () => {
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
   console.log("Server is running on port 5000");
   connectMongoDB();
 });
